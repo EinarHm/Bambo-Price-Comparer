35e573ad3d088b898e9953b8757690eda03f2c55
EinarHm <eihoma94@gmail.com>
2021-03-03 13:11:51 -0600

    Creating header styles

--- src/components/Headers/components/SearchDesktopHeader.js
+++ src/components/Headers/components/SearchDesktopHeader.js
@@ -2,7 +2,11 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 
 const SearchDesktopHeader = (props) => {
-  return <h1>This is a Header</h1>;
+  return (
+    <div className="header__desktop row">
+      <h1>This is a Header</h1>
+    </div>
+  );
 };
 
 export default withRouter(SearchDesktopHeader);
